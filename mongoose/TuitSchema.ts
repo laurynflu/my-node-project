d9e60dc3185fff2300f1f49fec38e7970c8c52ce
laurynflu <98038908+laurynflu@users.noreply.github.com>
2022-10-09 01:02:49 -0400

    Add files via upload

---
+++ mongoose/TuitSchema.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const TuitSchema = new mongoose.Schema({
+    tuit: String,
+    postedOn: Date,
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserModel'},
+
+}, {collection: 'tuits'});
+export default TuitSchema;
