ba9aec5342f5d56a759cd60bfebf39f0fcf115a5
laurynflu <98038908+laurynflu@users.noreply.github.com>
2022-10-26 16:28:02 -0400

    Add files via upload

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 /**
  * @file Implements an Express Node HTTP server.
  */
-import express, {Request, Response} from 'express';
+import * as express from 'express';
+import {Request, Response} from 'express';
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
 import LikeController from "./controllers/LikeController";
@@ -9,7 +10,6 @@ import mongoose from "mongoose";
 import FollowController from "./controllers/FollowController";
 import BookmarkController from "./controllers/BookmarkController";
 import MessageController from "./controllers/MessageController";
-var cors = require('cors')
 
 
 const options = {
@@ -22,11 +22,11 @@ const options = {
     family: 4
 }
 
-mongoose.connect('mongodb://localhost:27017/fse-tuiter', options);
+mongoose.connect('mongodb+srv://lflu2:FJCWMN7z4s0vXCIh@cluster0.tjrosmd.mongodb.net/?retryWrites=true&w=majority', options);
+
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 app.get('/', (req: Request, res: Response) =>
     res.send('Welcome!'));
@@ -43,5 +43,5 @@ MessageController.getInstance(app);
  * Start a server listening at port 4000 locally
  * but use environment variable PORT on Heroku if available.
  */
-const PORT = 4000;
+const PORT = 4002;
 app.listen(process.env.PORT || PORT);
